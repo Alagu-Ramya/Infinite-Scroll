b9f2c786edc808ef76440d99f9c4deae4e68c860
alex <alagumeena98@gmail.com>
2020-11-01 23:37:08 +0530

    Final commit of infinite scroll

--- script.js
+++ script.js
@@ -1,62 +1,87 @@
-const imageContainer = document.getElementById('image-container');
-const loader = document.getElementById('loader');
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
 
+let ready=false;
+let imagesLoaded = 0;
+totalImages= 0;
 let photosArray = [];
 
 //Unsplash API
-const count = 10;
-const apiKey = 'Tr2LPl21xuIeFfh1ZHJjT8yu5ckLxSYuUd61kG2dM0A';
+const count = 30;
+const apiKey = "iT1k2XsnPcCQBgg9kyKywm5cXJT9hrY29POYEYsfWbY";
+//TTnqs8znt9DOXVJKCAuMljnYDyMqAWVa6RIQEoTXxc8
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
-//Helper Function to set attributes on DOM Elements
-function setAttributes(element, attributes){
-    for (const key in attributes){
-        element.setAttribute(key, attributes[key]);
+//Check if all images were loaded
+function imageLoaded(){
+    imagesLoaded++;
+   // console.log(imagesLoaded);
+    if(imagesLoaded === totalImages){
+        ready = true;
+        loader.hidden = true;
+     //   console.log('ready =', ready);
     }
+}
+
+//Helper Function to set attributes on DOM Elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
 }
 
 //Create elements for Links & photos,Add to DOM
-function displayPhotos(){
+ function displayPhotos() {
+  try {
+      imagesLoaded = 0;
+      totalImages= photosArray.length;
+     // console.log('total images', totalImages);
     //Run function for each object in photosArray
-    photosArray.forEach((photo) =>{
-        console.log(photo);
-        //Create <a> to link to Unsplash
-        const item = document.createElement('a');
-        // item.setAttribute('href, photo.links.html');
-        // item.setAttribute('target','_blank');
-        setAttributes(item, {
-            href: photo.links.html,
-            target: '_blank',
-        });
-        //Create <img> for photo
-        const img = document.createElement('img');
-        // img.setAttribute('src', photo.urls.regular);
-        // img.setAttribute('alt', photo.alt_description);
-        // img.setAttribute('title', photo.alt_description);
-        setAttributes(img, {
-            src: photo.url.regular,
-            alt: photo.alt_description,
-            title: photo.alt_description,
-        });
-        console.log(img);
-        //Put <img> inside <a>, then put both inside imageContainer element
-        item.appendChild(img);
-        imageContainer.appendChild(item);
+    photosArray.forEach((photo, index) => {
+      //Create <a> to link to Unsplash
+      const item = document.createElement("a");
+      setAttributes(item, {
+        href: photo.links.html,
+        target: "_blank",
+      });
+      //Create <img> for photo
+      const img = document.createElement("img");
+      setAttributes(img, {
+        src: photo.urls.regular,
+        alt: photo.alt_description,
+        title: photo.alt_description,
+      });
+      // Event Listener, check when each is finished loading
+      img.addEventListener('load', imageLoaded);
 
-});
+      //Put <img> inside <a>, then put both inside imageContainer element
+      item.appendChild(img);
+      imageContainer.appendChild(item);
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+//Check to see if scrolling near bottom page, Load more photos
+window.addEventListener('scroll',() => {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready ){
+        ready=false;
+        getPhotos();
+        
+    }
+    
+});
 
 // Get photos from Unsplash API
 async function getPhotos() {
-    try {
-        const response = await fetch(apiURL);
-        photosArray = await response.json();
-        console.log(photosArray);
-        displayPhotos();
-        
-    } catch (error) {
-        //Catch error
-    }
+  try {
+    const response = await fetch(apiURL);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (error) {
+    //Catch error
+  }
 }
 
 //On Load
